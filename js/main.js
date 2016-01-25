17b9404cce7a2fe676baf90f05d34e13f276baba
Justin Fiore <justin.fiore@gmail.com>
2016-01-24 21:19:34 -0500

    Added some debug notifications

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ var app = {
         var self = this;
         // Check of browser supports touch events...
         if (document.documentElement.hasOwnProperty('ontouchstart')) {
+            self.showAlert("Registering touch events");
             // ... if yes: register touch event listener to change the "selected" state of the item
             $('body').on('touchstart', 'a', function(event) {
                 $(event.target).addClass('tappable-active');
@@ -19,6 +20,7 @@ var app = {
             $('body').on('touchend', 'a', function(event) {
                 $(event.target).removeClass('tappable-active');
             });
+            self.showAlert("Registered touch events");
         } else {
             // ... if not: register mouse events instead
             $('body').on('mousedown', 'a', function(event) {
